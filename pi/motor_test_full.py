ad661c519672e045471799a34475bd0a3ff600f6
Sparsh <bhatabhaiyya@gmail.com>
2025-07-24 03:55:42 +0100

    Experimenting with the servo on the test script; added an option to arm the ESC at the beginning (apparently that's necessary)

--- pi/motor_test_full.py
+++ pi/motor_test_full.py
@@ -9,10 +9,12 @@ from adafruit_pca9685 import PCA9685
 parser = argparse.ArgumentParser(description="Test ESC and Servo via PCA9685")
 parser.add_argument('-e', '--esc', type=int, default=1, help='ESC channel (default: 1)')
 parser.add_argument('-s', '--servo', type=int, default=0, help='Servo channel (default: 0)')
+parser.add_argument('-i', '--initialize', type=int, default = "", help='Initialize (default: 0)')
 args = parser.parse_args()
 
 ESC_CHANNEL = args.esc
 SERVO_CHANNEL = args.servo
+arm_ESC = args.initialize
 
 
 # === Constants ===
@@ -32,9 +34,15 @@ def pulse_to_duty(pulse_us):
 
 
 try:
-
-    for angle_pulse in range(1500, 2100, 100):
-        for throttle_pulse in range(1600, 2000, 100):
+    if (arm_ESC == 1):
+    	print("Arming the ESC...")
+    	pca.channels[ESC_CHANNEL].duty_cycle = pulse_to_duty(STOP_US)
+    	time.sleep(2)
+    	print("ESC armed. Initiating test...")
+    	time.sleep(0.5)
+    for angle_pulse in range(1500, 900, -50):
+        '''
+	for throttle_pulse in range(1000, 2100, 125):
             print(f"Setting servo to {angle_pulse} µs and ESC to {throttle_pulse} µs...")
             pca.channels[SERVO_CHANNEL].duty_cycle = pulse_to_duty(angle_pulse)
             pca.channels[ESC_CHANNEL].duty_cycle = pulse_to_duty(throttle_pulse)
@@ -45,9 +53,10 @@ try:
             pca.channels[SERVO_CHANNEL].duty_cycle = pulse_to_duty(angle_pulse)
             pca.channels[ESC_CHANNEL].duty_cycle = pulse_to_duty(throttle_pulse)
             time.sleep(0.5)
-
-    
-    print("Step 4: Back to neutral...")
+	'''
+    	pca_channels[SERVO_CHANNEL].duty_cycle = pulse_to_duty(angle_pulse)
+    	print('PWM Pulse: {}'.format(angle_pulse))
+    print("All tests done. Going back to neutral...")
     pca.channels[ESC_CHANNEL].duty_cycle = pulse_to_duty(STOP_US)
     pca.channels[SERVO_CHANNEL].duty_cycle = pulse_to_duty(STOP_US)
 
