bef6bd93f95447960726ee0a454c42a4235ce7c2
SparshJohri2023 <sparshjohri2004@gmail.com>
2025-07-29 10:50:55 -0700

    Changed the pull_recent_photos script to take in the network as a parameter

--- local/pull_recent_photos.sh
+++ local/pull_recent_photos.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # --- Usage Check ---
-if [[ $# -ne 4 ]]; then
-    echo "Usage: $0 <pi-user> <pi-ip-file> <remote-photo-dir> <local-dest-dir>"
-    echo "Example: $0 pi pi_ip.txt /home/pi/photos ~/Downloads/from-pi"
+if [[ $# -ne 5 ]]; then
+    echo "Usage: $0 <pi-user> <pi-ip-file> <network> <remote-photo-dir> <local-dest-dir>"
+    echo "Example: $0 pi pi_ip.txt home /home/pi/photos ~/Downloads/from-pi"
     exit 1
 fi
 
 PI_USER="$1"
 PI_IP_FILE="$2"
-REMOTE_DIR="$3"
-LOCAL_DIR="$4"
+NETWORK="$3"
+REMOTE_DIR="$4"
+LOCAL_DIR="$5"
 
 # --- Read IP address from file ---
 if [[ ! -f "$PI_IP_FILE" ]]; then
@@ -18,7 +19,12 @@ if [[ ! -f "$PI_IP_FILE" ]]; then
     exit 2
 fi
 
-PI_HOST=$(head -n 1 "$PI_IP_FILE")
+PI_HOST=$(awk -v net="$NETWORK" '$1 == net { print $2; exit }' "$PI_IP_FILE")
+if [[ -z "$PI_HOST" ]]; then
+    echo "Error: Network '$NETWORK' not found in '$PI_IP_FILE'."
+    exit 3
+fi
+
 PI_ADDR="${PI_USER}@${PI_HOST}"
 
 # --- Ensure local destination exists ---
